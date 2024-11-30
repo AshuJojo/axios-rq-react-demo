3767780f4177e08976aaf9d3a8b4139fc31b335b
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-11-30 12:37:10 +0530

    ProductList UI Implemented

--- src/api/fakeStoreApi.ts
+++ src/api/fakeStoreApi.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import Product from "../types/Product";
 
 const BASE_URL: string = "https://fakestoreapi.com";
 
@@ -6,9 +7,9 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getAllProducts = async () => {
+export const getAllProducts = async (): Promise<Product[] | null> => {
   try {
-    const res = await axiosInstance.get("/products");
+    const res: AxiosResponse<Product[]> = await axiosInstance.get("/products");
     return res.data;
   } catch (err) {
     console.error(err);
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,19 +3,47 @@ import { getAllProducts } from "../api/fakeStoreApi";
 import Product from "../types/Product";
 
 const ProductList = () => {
-  const { data } = useQuery({
+  const { data, isLoading, error } = useQuery<Product[] | null, Error>({
     queryKey: ["products"],
     queryFn: getAllProducts,
   });
 
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error.message}</p>;
+
   return (
     <div>
-      {data &&
-        data.map((product: Product) => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
-          </div>
-        ))}
+      <h2 className="text-[1.2rem] text-neutral-900 font-semibold pb-2">
+        Product List:
+      </h2>
+      <div className="grid grid-cols-4 gap-4">
+        {data &&
+          data.map((product: Product) => (
+            <div
+              key={product.id}
+              className="bg-neutral-200 rounded-lg flex flex-col gap-y-2 p-2"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                className="w-full h-[300px] object-cover rounded-md"
+              />
+              <div>
+                <p className="text-[0.8rem] text-neutral-600">
+                  {product.category}
+                </p>
+                <h3 className="text-[1rem] text-neutral-800 font-medium">
+                  {product.title.length > 20
+                    ? `${product.title.slice(0, 20)}...`
+                    : product.title}
+                </h3>
+              </div>
+
+              <p className="text-[0.9rem] text-neutral-800">${product.price}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
