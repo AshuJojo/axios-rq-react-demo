b13eb13727ae69252de5ddc4e9541cfaa362d421
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-11-30 11:58:18 +0530

    React Query Client Created and provided to throughout the react application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <main className="bg-neutral-50 min-h-screen">
+        <div className="w-[1020px] mx-auto py-4">
+          <h1 className="text-[2rem] text-neutral-950 font-bold">
+            React Query with Axios
+          </h1>
+        </div>
+      </main>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
